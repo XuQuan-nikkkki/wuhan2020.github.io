6017da003f8e0e9a37ee6d51225ecfa84665398e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-29 05:33:49 +0000

    Deploy wuhan2020/wuhan2020.github.io to github.com/wuhan2020/wuhan2020.github.io.git:master

---
+++ sw.js
@@ -0,0 +1,43 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts(
+  "https://cdn.jsdelivr.net/npm/workbox-sw"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "source.957912f6.js",
+    "revision": "533f9898fc0d68f1f75968e0b48623a3"
+  },
+  {
+    "url": "source.a5f819ca.css",
+    "revision": "6a6a13451a65632ce0f20aa42e796fe3"
+  },
+  {
+    "url": "WuHan-love.e7cd8688.png",
+    "revision": "1693978eb70f8fbd13e1a60dfc1829a7"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
